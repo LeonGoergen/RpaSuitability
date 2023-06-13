06f928bc34b74daf4b6014bcb8b6fa5c9212edef
leong <leon.goergen@gmail.com>
2023-06-13 19:04:42 +0200

    refactoring

--- src/app/contact-page/contact.component.ts
+++ src/app/contact-page/contact.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-page',
+  selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form data:', this.contactForm.value);
+      // TODO: Implement your service call here
+      this.contactForm.reset();
+    } else {
+      console.log('Form is not valid.');
+    }
+  }
 }
