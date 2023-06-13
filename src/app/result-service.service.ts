25538296a7141ff490e3d06566fe3390fcae9581
Leon Görgen <leon.goergen@gmail.com>
2023-06-13 10:51:27 +0200

    result box framework finished

---
+++ src/app/result-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { resultTexts } from '../assets/resultTexts';
+import { resultInterface } from "../assets/result-interface";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContentService {
+  private contentRanges: resultInterface[] = resultTexts;
+
+  getContentByPercentage(percentage: number): resultInterface | null {
+    return this.contentRanges.find(
+      (range) => percentage >= range.min && percentage <= range.max
+    ) || null;
+  }
+}
