d81d6de1b9684a10b1ec6a102bdab70eba1586fc
leong <leon.goergen@gmail.com>
2023-07-15 12:05:36 +0200

    scroll up contact page

--- src/app/contact-page/contact.component.ts
+++ src/app/contact-page/contact.component.ts
@@ -10,5 +10,6 @@ export class ContactComponent {
 
   constructor(private fb: FormBuilder, private renderer: Renderer2) {
     this.renderer.setStyle(document.body, 'background', 'linear-gradient(270deg, rgba(17, 153, 142, 0.3) 0%, rgba(56, 239, 125, 0.3) 100%)');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
