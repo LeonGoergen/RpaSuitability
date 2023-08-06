341b47308e294ce4751baae00fffdb4a5c86a6f5
Leon Görgen <leon.goergen@gmail.com>
2023-08-06 15:32:30 +0200

    added backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,6 @@ import { ContentSliderComponent } from './home-page/content-slider/content-slide
 import {AboutUsComponent} from "./contact-page/about-us/about-us.component";
 import { HttpClientModule } from '@angular/common/http';
 import {DashboardComponent} from "./dashboard/dashboard.component";
-import { QuestionTableComponent } from './dashboard/question-table/question-table.component';
-import { PropertiesTableComponent } from './dashboard/properties-table/properties-table.component';
-import { ChartComponent } from './dashboard/chart/chart.component';
 
 @NgModule({
   declarations: [
@@ -35,9 +32,6 @@ import { ChartComponent } from './dashboard/chart/chart.component';
     ContentSliderComponent,
     AboutUsComponent,
     DashboardComponent,
-    QuestionTableComponent,
-    PropertiesTableComponent,
-    ChartComponent,
   ],
     imports: [
         BrowserModule,
